9b4fb844e80cb77e3efc94bce73316aebf5257c0
Jansi Gorle <183268676+GorleJansi@users.noreply.github.com>
2026-07-11 13:52:22 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource aws_instance test1{
-    instance_type=t2.micro
-    ami_id = "ami-01edba92f9036f76e"
-    vpc_security_group_ids = "sg-0708e7bc7b4f99963"
-    tags={
-        name = test-terraform
-        env = dev
+resource "aws_instance" "test1" {
+    instance_type = "t2.micro"
+    ami = "ami-01edba92f9036f76e"
+    vpc_security_group_ids = ["sg-0708e7bc7b4f99963"]
+    tags = {
+        name = "test-terraform"
+        env = "dev"
     }
 }
--- providers.tf
+++ providers.tf
@@ -1,6 +1,4 @@
-
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
